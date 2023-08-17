7c44f1d615f3b3db818747593b335648f4da4d0c
Romulo Felizola <felizolaromulo23@gmail.com>
2023-08-17 17:59:59 -0400

    agregamos el framework de css foundation

--- applications/home/urls.py
+++ applications/home/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('lista-prueba/', views.ModeloListView.as_view()),
     path('add/', views.PruebaCreateView.as_view(),name="prueba_add"),
     #prueba css foundation
-    path('add/', views.PruebaCreateView.as_view(),name="prueba_add"),
+    path('resumen-foundation/', views.ResumenFoundationView.as_view(),name="resumendo_foundation"),
 
 
 
